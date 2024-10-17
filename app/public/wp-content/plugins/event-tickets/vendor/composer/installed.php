67dceee85e3c043e4faeeea5b38d070a3011cf02
kossi973 <150068989+kossi973@users.noreply.github.com>
2024-10-17 18:02:56 +0200

    WP project for NS 1.0 - initial commit

---
+++ app/public/wp-content/plugins/event-tickets/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'the-events-calendar/event-tickets',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '8b8891e367bfd9e368ef38dc25f0d9fcaba4165c',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'the-events-calendar/event-tickets' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '8b8891e367bfd9e368ef38dc25f0d9fcaba4165c',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
